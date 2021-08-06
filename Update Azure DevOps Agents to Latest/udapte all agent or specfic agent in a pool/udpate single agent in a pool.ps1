fb58732d18c7f548ddacfaf1e74afeb3622d8b13
KOHITH REDDY <kohithreddy@gmail.com>
2021-08-06 15:15:03 +0530

    Update udpate single agent in a pool.ps1

--- Update Azure DevOps Agents to Latest/udapte all agent or specfic agent in a pool/udpate single agent in a pool.ps1
+++ Update Azure DevOps Agents to Latest/udapte all agent or specfic agent in a pool/udpate single agent in a pool.ps1
@@ -7,6 +7,7 @@ function Get-Authentication {
 
         $Token = [System.Convert]::ToBase64String([System.Text.Encoding]::ASCII.GetBytes(":$($PAT)"))
         $Header = @{authorization = "Basic $Token"}
+        Write-Host "Returning authentication context"
         return $Header
 }
 $Header = Get-Authentication
@@ -19,13 +20,16 @@ $AgentId = 234
 $Uri = "$($Organization)_apis/distributedtask/pools/$($PoolId)/messages?agentId=$($AgentId)&api-version=$($ApiVersion)"
 try 
 {
+            Write-Host "Updating the agent"
             $Response = Invoke-RestMethod -Uri $Uri `
                                           -Method Post `
                                           -ContentType "application/json" `
                                           -Headers $Header
+            Write-Host "Completed agent version update process"
         } 
         catch 
         {
+            Write-Host "Caught an exception while updating the agent"
             Write-Host "URI": $Uri
             Write-Host "Status code:" $_.Exception.Response.StatusCode.value__ 
             Write-Host "Exception reason:" $_.Exception.Response.ReasonPhrase

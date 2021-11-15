1d448790888bd0e1fcef9c8bfd79ce75955e0e50
kohith Reddy <kohith@microsoft.com>
2021-11-15 18:24:10 +0530

    Adding Java Web App with Gradle Build

---
+++ JavaWebApp Gradle Build/WebAppJavaGradle/src/main/java/com/cx/divya/springboot/BootGradleApplication.java
@@ -0,0 +1,12 @@
+package com.cx.divya.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootGradleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootGradleApplication.class, args);
+	}
+}
---
+++ JavaWebApp Gradle Build/WebAppJavaGradle/src/main/webapp/WEB-INF/jsp/welcome.jsp
@@ -0,0 +1,2 @@
+<link href="../css/loginstyle.css" rel="stylesheet" type="text/css">
+<h1>Welcome to Spring Boot</h1>
